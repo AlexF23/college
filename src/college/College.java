8615f4a041197e70b374ff5dd16e5a6848d6abf2
AlexF23 <152330796+AlexF23@users.noreply.github.com>
2024-03-30 18:23:03 +0000

    comments

--- src/college/College.java
+++ src/college/College.java
@@ -14,14 +14,16 @@ public class College {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //this will generate a report of courses from the database
         CourseReportGenerator reportGenerator = new CourseReportGenerator();
         List<Course> courses = reportGenerator.generateReport();
         for (Course course : courses) {
-            System.out.println("Course Name: " + course.getName() +
-                               ", Program: " + course.getProgram() +
-                               ", Enrolled: " + course.getStudentsEnrolled() +
-                               ", Lecturer: " + course.getLecturerName() +
-                               ", Room: " + course.getRoom());
+            //this will print out a report for the follwing DB data using getters
+            System.out.println("Course Name: " + course.getName() + //get students name
+                               ", Program: " + course.getProgram() + //getthe program
+                               ", Enrolled: " + course.getStudentsEnrolled() + //get the students enrolled
+                               ", Lecturer: " + course.getLecturerName() + //get the naame of the lecturer
+                               ", Room: " + course.getRoom()); // get the room of the course
     }
     }
 }
