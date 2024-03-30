f744d32134214d7afeff3598c42a455b39a772a1
AlexF23 <152330796+AlexF23@users.noreply.github.com>
2024-03-30 17:39:51 +0000

    Update CourseReportGenerator.java

--- src/college/CourseReportGenerator.java
+++ src/college/CourseReportGenerator.java
@@ -4,6 +4,12 @@
  */
 package college;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Alexander
